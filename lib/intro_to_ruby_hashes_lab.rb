7ff5955a1cdad7852e013ec300b0962964e59495
Anney Park <anneypark@gmail.com>
2020-07-22 05:47:49 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,16 +1,16 @@
 def new_hash
-  new_hash = {}
+  {}
 end
 
 def my_hash
- my_hash = {:ramen => "shin"}
+ {:ramen => "shin"}
 end
 
 def pioneer
- pioneer = {:name => "Grace Hopper"}
+ {:name => "Grace Hopper"}
 end
 
 
 def id_hash_generator(number)
- 
+  {id: number}
 end
